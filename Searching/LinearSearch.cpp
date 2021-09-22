113f873940678a7542a8b41cc012abeef7d4ec44
omkar-zare <omkarzare123@gmail.com>
2021-09-22 17:48:32 +0530

    clang-formated

--- Searching/LinearSearch.cpp
+++ Searching/LinearSearch.cpp
@@ -1,39 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int linerSearch(int arr[],int k,int s)
-{
-    if(s==0)
-        return -1;
-    for(auto i=0;i<=s;i++)
-    {
-        if(arr[i]==k)
-            return i;
-    }
+int linerSearch(int arr[], int k, int s) {
+  if (s == 0)
     return -1;
+  for (auto i = 0; i <= s; i++) {
+    if (arr[i] == k)
+      return i;
+  }
+  return -1;
 }
 
-int main()
-{
-    long int n,ele;
-    cout<<"Enter the array size:"<<endl;
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the the array Elements "<<endl;
-    for(auto i=0;i<=n-1;i++)
-    {
-        cin>>arr[i];
-    }
-    cout<<"Enter the element to be searched:";
-    cin>>ele;
+int main() {
+  long int n, ele;
+  cout << "Enter the array size:" << endl;
+  cin >> n;
+  int arr[n];
+  cout << "Enter the the array Elements " << endl;
+  for (auto i = 0; i <= n - 1; i++) {
+    cin >> arr[i];
+  }
+  cout << "Enter the element to be searched:";
+  cin >> ele;
 
-    int result=linerSearch(arr,ele,n-1);
-    if(result==-1)
-    {
-        cout<<"Element "<<ele<<" not Present!!!"<<endl;
-    }
-    else{
-        cout<<"Element "<<ele<<" found at index "<<result<<endl;
-    }
-return 0;
+  int result = linerSearch(arr, ele, n - 1);
+  if (result == -1) {
+    cout << "Element " << ele << " not Present!!!" << endl;
+  } else {
+    cout << "Element " << ele << " found at index " << result << endl;
+  }
+  return 0;
 }
--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,39 +1,32 @@
-#include<iostream>
+#include <iostream>
 
-void Selection_sort(int arr[],int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        int min=i;
-        for(int j=i+1;j<n;j++)
-        {
-            if(arr[j]<arr[i])
-            {
-                min=j;
-            }
-        }
-        int t=arr[i];
-        arr[i]=arr[min];
-        arr[min]=t;
+void Selection_sort(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    int min = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[i]) {
+        min = j;
+      }
     }
+    int t = arr[i];
+    arr[i] = arr[min];
+    arr[min] = t;
+  }
 }
 
-int main()
-{
-    int n;
-    std::cout<<"Enter the Array Size:";
-    std::cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        std::cin>>arr[i];
-    }
+int main() {
+  int n;
+  std::cout << "Enter the Array Size:";
+  std::cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> arr[i];
+  }
 
-    Selection_sort(arr,n);
-    std::cout<<"Sorted Array:"<<std::endl;
-    for(int i=0;i<=n-1;i++)
-    {
-        std::cout<<arr[i]<<" ";
-    }
-    return 0;
+  Selection_sort(arr, n);
+  std::cout << "Sorted Array:" << std::endl;
+  for (int i = 0; i <= n - 1; i++) {
+    std::cout << arr[i] << " ";
+  }
+  return 0;
 }
